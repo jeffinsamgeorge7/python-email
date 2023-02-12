23a4e385a9e9119c21427052084a20bf9c7b4b8a
JEFFIN SAM GEORGE <76589856+jeffinsamgeorge7@users.noreply.github.com>
2023-02-12 20:10:57 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import smtplib
+import socket
+
+sender_email = "sender@gmail.com"
+receiver_email = "reciver@gmail.com"
+password = "password"
+
+message = """\
+Subject: Test Email
+
+This is a test email sent using SMTP and sockets in Python."""
+
+try:
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
+    print("Email sent successfully")
+except (socket.gaierror, ConnectionRefusedError):
+    print("Failed to connect to the server")
+except smtplib.SMTPException as error:
+    print("Error: unable to send email:", error)
+finally:
+    server.quit()
